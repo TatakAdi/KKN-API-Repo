a7e67f80b04034d8ca14718b276d2c02329244ad
TatakAdi <tatakadisubagyo@gmail.com>
2025-05-31 16:06:17 +0800

    fix : SupabaseClient Config

--- controllers/product.js
+++ controllers/product.js
@@ -1,6 +1,5 @@
 const { PrismaClient } = require("@prisma/client");
 const supabase = require("../config/supabaseClient");
-const jwt = require("jsonwebtoken");
 
 const prisma = new PrismaClient();
 
